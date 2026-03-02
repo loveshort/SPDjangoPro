96442e0e634ac12e8c755f09483885af6000519c
顾明次 <271901088@qq.com>
2026-03-02 21:44:16 +0800

    feat:1.写一些DRF 各种写法

--- SPDjangoPro/settings.py
+++ SPDjangoPro/settings.py
@@ -70,14 +70,17 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     #分页配置
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 50,
     #渲染器（默认返回 JSON）
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     #认证配置
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     #权限配置
     'DEFAULT_PERMISSION_CLASSES': [
@@ -188,33 +191,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#DRF 全局配置
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 10,
-        'DATETIME_FORMAT': '%Y-%m-%d %H:%M:%S',
-        'DEFAULT_RENDERER_CLASSES': [
-            'rest_framework.renderers.JSONRenderer',
-            'rest_framework.renderers.BrowsableAPIRenderer',
-        ],
-        'DEFAULT_PARSER_CLASSES': [
-            'rest_framework.parsers.JSONParser',
-            'rest_framework.parsers.FormParser',
-            'rest_framework.parsers.MultiPartParser',
-        ],
-        'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-        ],
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework.authentication.TokenAuthentication',
-            'rest_framework.authentication.SessionAuthentication',
-            'rest_framework.authentication.BasicAuthentication',
-        ],
-        'DEFAULT_FILTER_BACKENDS': [
-            'rest_framework.filters.SearchFilter',
-        ],
-    },
-    
-}
--- app/views.py
+++ app/views.py
@@ -12,7 +12,128 @@ from rest_framework.decorators import api_view
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.generics import GenericAPIView
 
+
+# viewsets 视图类 - 开发
+class BookList(viewsets.ModelViewSet):
+    queryset = AppModel.objects.all()
+    serializer_class = AppSerializer
+    def get(self, request):
+        return self.list(request)
+    def post(self, request):
+        return self.create(request)
+    def put(self, request, pk):
+        return self.update(request, pk)
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+    def retrieve(self, request, pk):
+        return self.retrieve(request, pk)
+    def update(self, request, pk):
+        return self.update(request, pk)
+    def destroy(self, request, pk):
+        return self.destroy(request, pk)
+    def list(self, request):
+        return self.list(request)
+    def create(self, request):
+        return self.create(request)
+    def update(self, request, pk):
+        return self.update(request, pk)
+    def destroy(self, request, pk):
+        return self.destroy(request, pk)
+
+
+#mixins 视图类 - 开发
+class BookList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = AppModel.objects.all()
+    serializer_class = AppSerializer
+    def get(self, request):
+        return self.list(request)
+    def post(self, request):
+        return self.create(request)
+    def put(self, request, pk):
+        return self.update(request, pk)
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+        
+
+class BookList(ListCreateAPIView):
+    queryset = AppModel.objects.all()
+    serializer_class = AppSerializer
+    def get(self, request):
+        books = self.get_queryset()
+        serializer = self.serializer_class(instance=books, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk):
+        book = self.get_object()
+        serializer = self.serializer_class(instance=book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        book = self.get_object()
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
+
+class BookList(RetrieveUpdateDestroyAPIView):
+    queryset = AppModel.objects.all()
+    serializer_class = AppSerializer
+    def get(self, request, pk):
+        book = self.get_object()
+        serializer = self.serializer_class(instance=book)
+        return Response(serializer.data)
+    def put(self, request, pk):
+        book = self.get_object()
+        serializer = self.serializer_class(instance=book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        book = self.get_object()
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
+
+
+# GenericAPIView 视图类 - 开发
+class BookList(GenericAPIView):
+    queryset = AppModel.objects.all()
+    serializer_class = AppSerializer
+    def get(self, request):
+        books = self.get_queryset()
+        serializer = self.serializer_class(instance=books, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# APIView 视图类 - 开发
+class BookList(APIView):
+    def get(self, request):
+        books = AppModel.objects.all()
+        serializer = AppSerializer(instance=books, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = AppSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'POST'])
 def book_list(request):
