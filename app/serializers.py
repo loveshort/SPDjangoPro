0816e4b988bac4e3f43df43853ca66e173892d83
顾明次 <271901088@qq.com>
2026-03-04 09:40:17 +0800

    feat:1.添加两个字段

--- app/serializers.py
+++ app/serializers.py
@@ -8,8 +8,8 @@ class AppSerializer(serializers.ModelSerializer):
         #只读字段
         # read_only_fields = ['id', 'pub_time']
         # #写入字段
-        # write_only_fields = ['name', 'title', 'author', 'price']
-        # #排除字段
+        # write_only_fields = ['name', 'title', 'author', 'price' ]
+        # #排除字段 
         # exclude = ['id', 'pub_time']
         # #嵌套字段
         # nested_fields = {
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
 def movie_list(request):
     return HttpResponse("电影列表")
 
 def movie_detail(request,movie_id):
     return HttpResponse(f"电影详情页的{movie_id}")
+
+class 
+
+
+class MovieList(APIView):
+    def get(self, request):
+        return Response("电影列表")
